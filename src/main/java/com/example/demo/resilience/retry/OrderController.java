450bcd56fee5fd78040cbc66d241584592f352b7
up1 <somkiat.p@gmail.com>
2022-03-12 09:36:16 +0700

    Working with retry + time limit

--- src/main/java/com/example/demo/resilience/retry/OrderController.java
+++ src/main/java/com/example/demo/resilience/retry/OrderController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
 @RestController
 public class OrderController {
 
@@ -11,7 +13,7 @@ public class OrderController {
     private PaymentService paymentService;
 
     @GetMapping("/order/checkout")
-    public String checkout() {
+    public CompletableFuture<String> checkout() {
         // Checkout process
         // Call payment gateway
         return paymentService.pay();
--- src/main/java/com/example/demo/resilience/retry/PaymentService.java
+++ src/main/java/com/example/demo/resilience/retry/PaymentService.java
@@ -1,9 +1,14 @@
 package com.example.demo.resilience.retry;
 
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import io.vavr.control.Try;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeoutException;
 
 @Service
 public class PaymentService {
@@ -11,15 +16,16 @@ public class PaymentService {
     private static final Logger logger = LoggerFactory.getLogger(PaymentService.class);
 
     @Retry(name = "paymentService")
-    public String pay() {
-        return callPaymentGateway();
+    @TimeLimiter(name = "paymentService")
+    public CompletableFuture<String> pay() {
+        return CompletableFuture.supplyAsync(this::callPaymentGateway);
     }
 
     private String callPaymentGateway() {
         // Call Payment API
         logger.info("Call slow payment gateway");
-        throw new PaymentException();
+        Try.run(() -> Thread.sleep(5000));
+        return "Pay success";
     }
-
 
 }
